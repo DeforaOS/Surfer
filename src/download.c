d96951a93dfb3a082280bad561516ed90a1478bf
Pierre Pronchery <khorben@defora.org>
2014-01-20 11:58:31 -0600

    Added missing include for download(1) with WebKit (as by default)
    Reported by Joerg Sonnenberger of NetBSD, thanks!

--- src/download.c
+++ src/download.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2009-2012 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2009-2014 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS Desktop Surfer */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,6 +32,7 @@
 #include <gtk/gtk.h>
 #include <System.h>
 #ifdef WITH_WEBKIT
+# include <arpa/inet.h>
 # include <webkit/webkit.h>
 #else
 # define GNET_EXPERIMENTAL
