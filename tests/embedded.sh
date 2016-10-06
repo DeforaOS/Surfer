407f56cd1f04a1128f8045535aabc64ee0258451
Pierre Pronchery <khorben@defora.org>
2016-10-06 02:31:29 +0200

    Add test to build in embedded mode

---
+++ tests/embedded.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#$Id$
+#Copyright (c) 2016 Pierre Pronchery <khorben@defora.org>
+#This file is part of DeforaOS Desktop Surfer
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
+#variables
+PROGNAME="embedded.sh"
+TARGETS="all"
+#executables
+GCC="gcc"
+MAKE="make"
+MKTEMP="mktemp"
+RM="rm -f"
+
+
+#functions
+#embedded
+_embedded()
+{
+	cppflags="-DEMBEDDED"
+	make="$MAKE CPPFLAGS=\"$cppflags\""
+	objdir=$($MKTEMP -d)
+	[ $? -eq 0 ]						|| return 2
+
+	(cd .. && sh -c "$make OBJDIR='$objdir/' $TARGETS")
+	ret=$?
+	[ $ret -eq 0 ] || echo "$PROGNAME: $1: Could not build for embedded" 1>&2
+	#$RM -r -- "$objdir"
+	return $ret
+}
+
+
+#main
+_embedded							|| return 2
